bc774768b577042f35b5f6610af3b324365c7ab2
lulingxi <lingxiao.lu@mail.utoronto.ca>
2018-11-18 15:39:57 -0500

    Add randomized movement, more interesting!

--- src/tictactoe/AIMove.java
+++ src/tictactoe/AIMove.java
@@ -1,49 +1,48 @@
 package tictactoe;
 
-public class AIMove extends CommandMove{
-	
+public class AIMove extends CommandMove {
+
 	private GameTreeNode curr;
-	
+
 	public AIMove(GameBoard board, int pNum, GameTreeNode curr) {
-		super(board,pNum);
+		super(board, pNum);
 		this.curr = curr;
 	}
 
-	
 	public int decide() {
-		int bestChoice=-1;
+		int bestChoice = -1;
 		double bestWinRate = 0, bestDrawRate = 0;
 		System.out.println("Win Rates:");
-		for (int i=0;i<9;i++) {
-			if (curr.child[i]!= null) {
-				System.out.printf(""+i+": %.3f ",curr.child[i].winRate);
-				if (curr.child[i].winRate>bestWinRate) {
-					bestWinRate=curr.child[i].winRate;
-					bestChoice=i;
+		for (int i = 0; i < 9; i++) {
+			if (curr.child[i] != null) {
+				System.out.printf("" + i + ": %.3f ", curr.child[i].winRate);
+				if (curr.child[i].winRate > bestWinRate) {
+					bestWinRate = curr.child[i].winRate;
+					bestChoice = i;
 				}
 			}
 		}
 		System.out.println();
 		if (bestChoice != -1) {
-			return bestChoice;
+			return randomizedChoice(0, bestWinRate);
 		}
-		System.out.println("\nDraw rates:");
-		for (int i=0;i<9;i++) {
-			if (curr.child[i]!= null) {
-				System.out.printf(""+i+": %.3f ",curr.child[i].drawRate);
-				if (curr.child[i].drawRate>bestDrawRate) {
-					bestDrawRate=curr.child[i].drawRate;
-					bestChoice=i;
+		System.out.println("Draw rates:");
+		for (int i = 0; i < 9; i++) {
+			if (curr.child[i] != null) {
+				System.out.printf("" + i + ": %.3f ", curr.child[i].drawRate);
+				if (curr.child[i].drawRate > bestDrawRate) {
+					bestDrawRate = curr.child[i].drawRate;
+					bestChoice = i;
 				}
 			}
 		}
 		System.out.println();
 		if (bestChoice != -1) {
-			return bestChoice;
+			return randomizedChoice(1, bestDrawRate);
 		}
 		System.out.println("I lose.");
-		for (int i=0;i<9;i++) {
-			if (curr.child[i]!= null) {
+		for (int i = 0; i < 9; i++) {
+			if (curr.child[i] != null) {
 				return i;
 			}
 		}
@@ -52,22 +51,51 @@ public class AIMove extends CommandMove{
 
 	@Override
 	public int move() {
-		
-		int choice=decide();
+
+		int choice = decide();
 		if (choice != -1) {
-			curr=curr.child[choice];
+			curr = curr.child[choice];
 			board.setMove(choice, pNum);
 			System.out.println(board);
 		}
 		doIWin();
 		return 0;
 	}
-	
+
 	public void updatePlayerMove(int p) {
 		if (curr.child[p] == null) {
 			System.out.println("This game does not exist! What happened!");
 		} else {
-			curr=curr.child[p];
+			curr = curr.child[p];
 		}
+	}
+
+	/**
+	 * return a randomized choice for equally good choices
+	 * 
+	 * @param type 0 = win, 1 = draw
+	 */
+	public int randomizedChoice(int type, double rate) {
+		int x;
+		if (type == 0) {
+			x = (int) (Math.random() * 9);
+			while (true) {
+				if (curr.child[x] != null) {
+					if (curr.child[x].winRate == rate)
+						break;
+				}
+				x = (int) (Math.random() * 9);
+			}
+		} else {
+			x = (int) (Math.random() * 9);
+			while (true) {
+				if (curr.child[x] != null) {
+					if (curr.child[x].drawRate == rate)
+						break;
+				}
+				x = (int) (Math.random() * 9);
+			}
+		}
+		return x;
 	}
 }
